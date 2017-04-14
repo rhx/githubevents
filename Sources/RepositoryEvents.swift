c258e25491dee54236ddc8caa7cf7d77c19883f2
Rene Hexel <r.hexel@griffith.edu.au>
2017-04-14 11:46:05 +1000

    Fetch and display github events for the given repository

---
+++ Sources/RepositoryEvents.swift
@@ -0,0 +1,41 @@
+//
+//  RepositoryEvents.swift
+//  githubevents
+//
+//  Created by Rene Hexel on 14/4/17.
+//
+import Foundation
+import RxSwift
+import RxCocoa
+
+/// Observable repository events
+class RepositoryEvents {
+    let bag: DisposeBag
+    let url: URL
+    init?(uri: String) {
+        guard let u = URL(string: uri) else { return nil }
+        url = u
+        bag = DisposeBag()
+    }
+
+    /// Asynchronously fetch events
+    ///
+    /// - Parameter processEvents: callback for processing received events
+    func fetchEvents(_ processEvents: @escaping ([Event]) -> Void) {
+        let response = Observable.from([url])
+            .map { URLRequest(url: $0) }
+            .flatMap { URLSession.shared.rx.response(request: $0) }
+            //.shareReplay(1)
+
+        response.filter { 200..<300 ~= $0.0.statusCode }
+            .map { _, data -> [[String : Any]] in
+                guard let jsonObject = try? JSONSerialization.jsonObject(with: data, options: []),
+                      let dictionaries = jsonObject as? [[String: Any]] else { return [] }
+                return dictionaries
+            }
+            .filter { $0.count > 0 }
+            .map { $0.flatMap(Event.init) }
+            .subscribe(onNext: { processEvents($0) })
+            .addDisposableTo(bag)
+    }
+}
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,21 @@
-print("Hello, world!")
+import Foundation
+import Dispatch
+import RxSwift
+
+let repo: String
+if CommandLine.arguments.count > 1 {
+    repo = CommandLine.arguments[1]
+} else {
+    repo = "rhx/githubevents"
+}
+let eventsURI = "https://api.github.com/repos/\(repo)/events"
+let handler = RepositoryEvents(uri: eventsURI)
+
+handler?.fetchEvents {
+    for event in $0 {
+        print("\(event.name): \(event.action)")
+    }
+    exit(EXIT_SUCCESS)
+}
+
+dispatchMain()
